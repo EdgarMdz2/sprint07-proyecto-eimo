373bafc4cc37bc2d765601f5bb125eb344d9756f
EdgarMdz2 <edgarmendeez@gmail.com>
2025-04-28 13:29:15 -0600

    V2 - Actualización de app.py

--- app.py
+++ app.py
@@ -4,23 +4,36 @@ import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('Proyecto Sprint-07')
-hist_checkbox = st.checkbox('Construir histograma')
-scatter_checkbox = st.checkbox('Construir gráfico de dispersión')
+st.caption('Edgar Iván Méndez Ovando - Proyecto Sprint 7')
+st.header('Visualización de datos.')
+st.subheader('Autos a la venta anunciados.')
+
+hist_checkbox = st.checkbox('Construir histograma (Kilometraje)')
+scatter_checkbox = st.checkbox('Gráfico de dispersión (Año - Kilometraje)')
 
 if hist_checkbox:
     st.write(  # escribir un mensaje
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        'Histograma - Frecuencia por kilometraje')
     # crear un histograma
-    fig1 = px.histogram(car_data, x='odometer')
+    fig1 = px.histogram(car_data,
+                        x='odometer',
+                        labels={
+                            'odometer': 'Kilometraje (millas)'
+                        })
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig1, use_container_width=True)
 
 
 if scatter_checkbox:
     st.write(  # escribir un mensaje
-        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+        'Dispersión "Año - Kilometraje"')
     # crear gráfico de dispersión
-    fig2 = px.scatter(car_data, x="odometer", y="price")
+    fig2 = px.scatter(car_data,
+                      x="model_year",
+                      y="odometer",
+                      labels={
+                          'odometer': 'Kilometraje (millas)',
+                          'model_year': 'Modelo (año)'
+                      })
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig2, use_container_width=True)
